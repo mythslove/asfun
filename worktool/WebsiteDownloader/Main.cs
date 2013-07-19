eba212fee2195ca0260acae1e8da781a281e2819
riasite@gmail.com <riasite@gmail.com@664f4500-c33f-11dd-b31d-17274aa020d1>
2013-07-19 12:43:56 +0000

    修改一些出BUG的情况

--- worktool/WebsiteDownloader/Main.cs
+++ worktool/WebsiteDownloader/Main.cs
@@ -48,27 +48,59 @@ namespace WebsiteDownloader
                 url = url.Substring(0, qIndex);
             }
 
+            //有些网址是是类似http://www.baidu.com/，这样的网址没有名字，但我要给他一个默认名
+            string tt = url.Substring(url.Length - 2);
+            if (url.Substring(url.Length - 1) == "/")
+            {
+                this.addLog(LogType.TIPS, "因为URL\"" + url + "\"没有找到文件名，所以使用默认文件名\"index.html\""); 
+                url += "index.html";
+            }
+
             url = url.Replace(":", "：");
             url = "WebsiteFile/" + url;
             qIndex = url.LastIndexOf("/");
             string path = url.Substring(0, qIndex);
             string name = url.Substring(qIndex + 1);
 
-            if (name == "") return;
-
-            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-
-            byte[] data = oSession.responseBodyBytes;
-            if (autoDezipMI.Checked && GZipTool.IsGZipFile(data))
-            {
-                data = GZipTool.Decompress(data);
-                File.WriteAllBytes(url, data);
-                this.addLog("保存了文件：" + url + "(GZip)");
+            if (name == ""){
+                this.addLog(LogType.ERROR, "没解析出文件名，地址是：" + url);
+                return;
             }
-            else
+
+            if (!Directory.Exists(path))
             {
-                File.WriteAllBytes(url, data);
-                this.addLog("保存了文件：" + url);
+                try
+                {
+                    Directory.CreateDirectory(path);
+                }
+                catch (Exception e)
+                {
+                    this.addLog(LogType.ERROR, e.Message + ", 地址是：" + url);
+                    return;
+                }
+            }
+
+            try
+            {
+
+                byte[] data = oSession.responseBodyBytes;
+                if (autoDezipMI.Checked && GZipTool.IsGZipFile(data))
+                {
+                    data = GZipTool.Decompress(data);
+                    File.WriteAllBytes(url, data);
+                    this.addLog("保存了文件：" + url + "(GZip)");
+                }
+                else
+                {
+                    File.WriteAllBytes(url, data);
+                    this.addLog("保存了文件：" + url);
+                }
+
+            }
+            catch (Exception e)
+            {
+                this.addLog(LogType.ERROR, e.Message + ", 地址是：" + url);
+                return;
             }
             
             
@@ -103,6 +135,24 @@ namespace WebsiteDownloader
             {
                 logTxt.AppendText(txt + "\n");
                 logTxt.ScrollToCaret();
+            }
+        }
+
+        public void addLog(LogType type, string txt)
+        {
+            switch (type)
+            {
+                case LogType.NONE:
+                    this.addLog(txt);
+                    break;
+
+                case LogType.ERROR:
+                    this.addLog("错误:" + txt);
+                    break;
+
+                case LogType.TIPS:
+                    this.addLog("提示:" + txt);
+                    break;
             }
         }
 
@@ -144,5 +194,18 @@ namespace WebsiteDownloader
             newForm.ShowDialog(this);
         }
 
+    }
+
+
+    public enum LogType
+    {
+        [Description("普通的")]
+        NONE,
+
+        [Description("错误")]
+        ERROR,
+
+        [Description("提示")]
+        TIPS
     }
 }
